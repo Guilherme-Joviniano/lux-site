a5688f80b0f2cc57ccc74963feaf626cb302092c
Guilherme Joviniano de Sousa <00drpixelss@gmail.com>
2022-05-18 22:51:54 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,14 @@ const navbar = document.querySelector('nav')
 const navbarCarreiras = document.querySelector('.navbar-carrers')
 const btnCurriculo = document.querySelector(".btn-curriculo")
 
+const btnReq = document.querySelector(".btn-req")
+const btnDef = document.querySelector(".btn-des")
+
+const reqLista = document.querySelector(".info-req")
+const defLista = document.querySelector(".info-def")
 
 
-
+console.log(defLista);
 btnNav.addEventListener("click", function () {
     if (navbarCarreiras != null) {
         if (navbarCarreiras.style.display == "flex") {
@@ -27,7 +32,28 @@ btnNav.addEventListener("click", function () {
 })
 
 
-btnCurriculo.addEventListener('click', function(){
-    
+btnCurriculo.addEventListener('click', function () {
     window.location.href = "mailto:?subject=Subject&body=" + message;
+})
+
+
+console.log(btnDef, defLista);
+
+btnReq.addEventListener("click", function () {
+    if (reqLista.style.display === "flex") {
+        reqLista.style.display = "none"
+        defLista.style.display = "none"
+    } else {
+        defLista.style.display = "none"
+        reqLista.style.display = "flex"
+    }
+})
+btnDef.addEventListener("click", function () {
+    if (defLista.style.display === "flex") {
+        defLista.style.display = "none"
+        reqLista.style.display = "none"
+    } else {
+        reqLista.style.display = "none"
+        defLista.style.display = "flex"
+    }
 })
