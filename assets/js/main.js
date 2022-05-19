75a0adc90397ee1b57f7a054ddb5fa078ed4c1b7
Guilherme Joviniano de Sousa <00drpixelss@gmail.com>
2022-05-18 23:39:17 -0300

    updating the vagas page, css and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,14 @@ const navbar = document.querySelector('nav')
 const navbarCarreiras = document.querySelector('.navbar-carrers')
 const btnCurriculo = document.querySelector(".btn-curriculo")
 
+const btnReq = document.querySelector(".btn-req")
+const btnDef = document.querySelector(".btn-des")
+
+const reqLista = document.querySelector(".info-req")
+const defLista = document.querySelector(".info-def")
 
 
-
+console.log(defLista);
 btnNav.addEventListener("click", function () {
     if (navbarCarreiras != null) {
         if (navbarCarreiras.style.display == "flex") {
@@ -27,7 +32,34 @@ btnNav.addEventListener("click", function () {
 })
 
 
-btnCurriculo.addEventListener('click', function(){
-    
+btnCurriculo.addEventListener('click', function () {
     window.location.href = "mailto:?subject=Subject&body=" + message;
+})
+
+
+console.log(btnDef, defLista);
+
+btnReq.addEventListener("click", function () {
+    if (reqLista.style.display === "flex") {
+        btnReq.style.backgroundColor = "#F4C3BD"
+        reqLista.style.display = "none"
+        defLista.style.display = "none"
+    } else {
+        btnReq.style.backgroundColor = "#9A5071"
+        btnDef.style.backgroundColor = "#F4C3BD"
+        defLista.style.display = "none"
+        reqLista.style.display = "flex"
+    }
+})
+btnDef.addEventListener("click", function () {
+    if (defLista.style.display === "flex") {
+        btnDef.style.backgroundColor = "#F4C3BD"
+        defLista.style.display = "none"
+        reqLista.style.display = "none"
+    } else {
+        btnDef.style.backgroundColor = "#9A5071"
+        btnReq.style.backgroundColor = "#F4C3BD"
+        reqLista.style.display = "none"
+        defLista.style.display = "flex"
+    }
 })
--- assets/js/vagas/vagas.js
+++ assets/js/vagas/vagas.js
@@ -1,4 +1,5 @@
-function Vagas(requisitos, desafios, area, name) {
+function Vagas(requisitos, desafios, area, name, id) {
+    this.id = id;
     this.area = area;
     this.name = name;
     this.requisitos = requisitos;
@@ -10,8 +11,13 @@ function Vagas(requisitos, desafios, area, name) {
 Vagas.prototype.criarElemento = function () {
     const area = document.querySelector(".area-vaga")
     area.innerHTML = `<h1>${this.area}</h1>`
+
     const name = document.querySelector(".name-vaga")
     name.innerHTML = `<h2>${this.name}</h2>`
+
+    const picture = document.querySelector(".image-vaga")
+    picture.src = imgPath + `vaga-${this.id}.png`
+    console.log(this.id);
 
     const listaDesafios = document.querySelector(".info-def")
     const listaRequisitos = document.querySelector(".info-req")
@@ -34,11 +40,9 @@ Vagas.prototype.criarElemento = function () {
 }
 
 const urlJson = `https://guilherme-joviniano.github.io/lux-site/assets/js/vagas/vagas.json`;
-
-let vagaID = localStorage.getItem("vaga")
+const imgPath = `../assets/img/vagas/`
 
 function carregarElementosNaPagina(data, vagaID) {
-
     let requisitos = data[vagaID]['requisitos']
     let desafios = data[vagaID]['desafios']
     let area = data[vagaID]['area']
@@ -46,12 +50,13 @@ function carregarElementosNaPagina(data, vagaID) {
     const title = document.querySelector('title')
 
     title.innerHTML = `Vaga - ${name}`
-    new Vagas(requisitos, desafios, area, name)
-
+    new Vagas(requisitos, desafios, area, name, vagaID)
 }
+
+let vagaID = localStorage.getItem("vaga")
 
 fetch(urlJson)
     .then(response => {
-            return response.json();
-        })
+        return response.json();
+    })
     .then(data => carregarElementosNaPagina(data['Vagas'], vagaID));
